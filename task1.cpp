d8597fc1e4ecbbe69b78d1a5a82d192b3bfe04e3
chagyl <54790362+chagyl@users.noreply.github.com>
2023-11-04 01:48:58 +0900

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,26 @@
+/* 
+ 1. Напишите программу, которая вводит с клавиатуры одно целое число,
+    и выводит POL, если оно положительно, OTR — если отрицательно, и
+    NUL, если это нуль.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() 
+{
+	int x;
+	printf("Vvedite chislo : ");
+	scanf("%d", &x);
+	if (x > 0) 
+	{
+		printf("POL");
+	}
+	if (x < 0)
+	{
+		printf("OTR");
+	}
+	else if (x == 0)
+	{
+		printf("NUL");
+	}
+	return 0;
+}
